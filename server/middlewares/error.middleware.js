a7d0ca0e2092e7f8679a15c4edc2983df5d5ea59
Kasif17 <developer786kasif@gmail.com>
2026-07-07 15:42:34 +0530

    Add utility layer for API responses, errors, async handling, and JWT helpers

--- server/middlewares/error.middleware.js
+++ server/middlewares/error.middleware.js
@@ -1,17 +1,24 @@
 const errorHandler = (err, req, res, next) => {
 
-    const statusCode = err.statusCode || 500;
+    let statusCode = err.statusCode || 500;
+
+    let message = err.message || "Internal Server Error";
 
     res.status(statusCode).json({
 
         success: false,
 
-        message: err.message || "Internal Server Error",
+        message,
+
+        errors: err.errors || [],
 
         stack:
+
             process.env.NODE_ENV === "development"
+
                 ? err.stack
-                : null
+
+                : undefined
 
     });
 
---
+++ server/utils/constants.js
@@ -0,0 +1,19 @@
+export const USER_ROLE = {
+
+    USER: "USER",
+
+    ADMIN: "ADMIN"
+
+};
+
+export const FILE_TYPES = [
+
+    "application/pdf",
+
+    "text/plain",
+
+    "text/markdown"
+
+];
+
+export const MAX_FILE_SIZE = 5 * 1024 * 1024;
